cea7be11f00e66f8f8557ed72d17db1f7d986792
MyroslavShymon <myroslavshymon@gmail.com>
2023-09-27 17:05:58 +0300

    commit 4 (add mysql and postgresql connection)

--- src/orm/test-orm.ts
+++ src/orm/test-orm.ts
@@ -1,50 +1,110 @@
 import {Pool, Client, PoolConfig, PoolClient} from 'pg';
+import {ConnectionOptions} from "mysql2";
+import {Connection as BaseConnection} from "mysql2/typings/mysql/lib/Connection";
+const mysql = require('mysql2');
+
+
+export interface DataSourceInterface {
+    client
+    connect(dataToConnect: DataToConnect);
+}
+
+export class DataSourcePostgres implements DataSourceInterface {
+    // private pool: Pool;
+    client: PoolClient;
+
+    async connect(dataToConnect: DataToConnect) {
+        const pool = new Pool(dataToConnect);
+        this.client = await pool.connect();
+    }
+
+}
+
+export class DataSourceMySql implements DataSourceInterface {
+    client: BaseConnection;
+
+    connect(dataToConnect: DataToConnect) {
+        this.client = mysql.createConnection(dataToConnect);
+    }
+}
 
 export enum DatabasesTypes {
     POSTGRES = 'postgres',
     MYSQL = 'mysql'
 }
 
-export interface DataToConnect extends PoolConfig {
-    type: DatabasesTypes;
+export type DataToConnect = PoolConfig & ConnectionOptions & {
+    type: DatabasesTypes
 }
 
-class ORM {
-    private pool: Pool;
-    client: PoolClient;
+class DataSource {
+    private dataSource: DataSourceInterface;
+    client
 
-
-    async connectPostgres(dataToConnection: DataToConnect) {
-        this.pool = new Pool(dataToConnection);
-        this.client = await this.pool.connect()
+    setDatabase(dataSource: DataSourceInterface) {
+        this.dataSource = dataSource;
     }
 
-
+    async connectDatabase(dataToConnect: DataToConnect) {
+        await this.dataSource.connect(dataToConnect);
+        this.client = this.dataSource.client;
+    }
 }
 
+export async function testConnection(type: DatabasesTypes) {
+    const dataSource = new DataSource();
+    if (type === DatabasesTypes.POSTGRES) {
+        dataSource.setDatabase(new DataSourcePostgres());
 
+        await dataSource.connectDatabase({
+            type: DatabasesTypes.POSTGRES,
+            user: 'postgres',
+            host: 'localhost',
+            database: 'postgres',
+            password: 'myrosia2016',
+            port: 5432,
+        })
 
-export async function testConnection() {
-    const orm = new ORM()
-    await orm.connectPostgres({
-        type: DatabasesTypes.POSTGRES,
-        user: 'postgres',
-        host: 'localhost',
-        database: 'postgres',
-        password: 'myrosia2016',
-        port: 5432,
-    })
-
-    console.log("test test test 2")
-
-    try {
-        const {rows} = await orm.client.query('SELECT current_user')
-        const currentUser = rows[0]['current_user'];
-        console.log('currentUser', currentUser)
-    } catch (e) {
-        console.error(e);
+        try {
+            const {rows} = await dataSource.client.query('SELECT current_user')
+            const currentUser = rows[0]['current_user'];
+            console.log('currentUser', currentUser)
+        } catch (e) {
+            console.error(e);
+        }
+        finally {
+            dataSource.client.client.release();
+        }
     }
-    finally {
-        orm.client.release();
+
+    if (type === DatabasesTypes.MYSQL) {
+        dataSource.setDatabase(new DataSourceMySql());
+
+        await dataSource.connectDatabase({
+            type: DatabasesTypes.MYSQL,
+            host: 'localhost',
+            user: 'root',
+            database: 'first'
+        })
+
+        dataSource.client.query(
+            "SELECT * FROM `table1`",
+            function(err, results, fields) {
+                console.log('resultsresultsresultsresults', results);
+                console.log('fielfieldsfieldsfieldsds', fields);
+                console.log('errerrerrerr', err)
+            }
+        );
     }
+    // const orm = new ORM()
+    // await orm.connectPostgres({
+    //     type: DatabasesTypes.POSTGRES,
+    //     user: 'postgres',
+    //     host: 'localhost',
+    //     database: 'postgres',
+    //     password: 'myrosia2016',
+    //     port: 5432,
+    // })
+    //
+    console.log("test test test 3")
 }
