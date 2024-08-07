47a9444785af3e36cbeb5f562ebe98c89d3179ab
MyroslavShymon <myroslavshymon@gmail.com>
2024-08-07 23:03:58 +0300

    commit 150 (fixing update queries)

--- src/orm/strategies/postgres/components/querying/update-queries.ts
+++ src/orm/strategies/postgres/components/querying/update-queries.ts
@@ -3,7 +3,7 @@ import { UpdateQueriesInterface } from '@strategies/common';
 export class UpdateQueries implements UpdateQueriesInterface {
 	update(values: Partial<Object>, tableName: string): string {
 		const setClause = Object.entries(values)
-			.map(([column]) => `${column} = '?'`)
+			.map(([column]) => `${column} = ?`)
 			.join(', ');
 
 		return `UPDATE ${tableName}
